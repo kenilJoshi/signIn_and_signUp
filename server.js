a13619e9f7e77bf89f74e4eb5192ad3738bfb57c
kenil <keniljoshi3@gmail.com>
2021-12-21 11:38:16 +0530

    make it better

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ require("./app/db/mongooseConnect");
 const cookieParser = require("cookie-parser");
 const { registerRoutes } = require("./routes/registerUser");
 const { loginRoutes } = require("./routes/loginUser");
+const port = process.env.PORT || 3000;
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(registerRoutes);
 app.use(loginRoutes);
 
-app.listen(3000, () => {
-  console.log("server is listening at 3000");
+app.listen(port, () => {
+  console.log(`server is listening at ${port}`);
 });
